7776e8c97c14a833658d46253b1651296f5fe690
Vineetha Kondepudi <vineethakondepudi22@example.com>
2025-05-19 12:47:11 +0530

    updated

--- back_end/index.js
+++ back_end/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const registrationRoute = require('./routing/router');
-const cors = require("cors");
-app.use(cors());
 
 // Import MongoDB connection
 require('./db/server');
@@ -14,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://first-care-1.onrender.com", // your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 // Routes
