89b62647d51f51ae49591725e46472b47c0d96f9
Vineetha Kondepudi <vineethakondepudi22@example.com>
2025-05-24 11:41:15 +0530

    updated

--- back_end/model/DropdownOptionsSchema.js
+++ back_end/model/DropdownOptionsSchema.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const DropdownOptionsSchema = new mongoose.Schema({
-  specialization: String,
-  location: Number,
-  language: String,
-  feeRange: String,
-  availability: String, 
-});
+  specialization: [String],
+  location: [Number],
+  language: [String],
+  feeRange: [String],
+  availability: [String]
+}, { timestamps: true });
+
 
 module.exports = mongoose.model('patientDropdown', DropdownOptionsSchema);
   
