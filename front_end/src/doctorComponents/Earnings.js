1240874340b77dd574550774b122c7060ce4cbd0
Vineetha Kondepudi <vineethakondepudi22@example.com>
2025-05-24 15:33:23 +0530

    updated

--- front_end/src/doctorComponents/Earnings.js
+++ front_end/src/doctorComponents/Earnings.js
@@ -4,6 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import { Button, DatePicker, Space, Pagination } from 'antd';
 import { ArrowRightOutlined } from '@ant-design/icons';
 import dayjs from 'dayjs';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
 
 const Earnings = () => {
    
@@ -52,12 +57,12 @@ const paginatedData = filteredRecords.slice(
       })
       .catch(err => console.error('Error fetching records:', err));
   }, []);
-  
+
 const handleFilter = () => {
   if (!fromDate || !toDate) return;
 
-  const from = dayjs(fromDate).startOf('day');  // 00:00:00
-  const to = dayjs(toDate).endOf('day');        // 23:59:59
+  const from = dayjs(fromDate).startOf('day');
+  const to = dayjs(toDate).endOf('day');
 
   const filtered = records.filter(record => {
     const recordDate = dayjs(record.date);
@@ -65,8 +70,9 @@ const handleFilter = () => {
   });
 
   setFilteredRecords(filtered);
-  setCurrentPage(1); // Reset to first page
+  setCurrentPage(1);
 };
+
 
 
   return (
